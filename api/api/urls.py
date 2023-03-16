d92de91e916afbf7cdf51ec851936ff537c763d4
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-03-16 18:20:56 +0530

    Update urls.py

--- api/api/urls.py
+++ api/api/urls.py
@@ -22,5 +22,5 @@ from core import urls as core_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('todos/', include(todo_urls)),
+    path('todos/', include(core_urls)),
 ]

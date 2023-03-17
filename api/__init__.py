64774f3588f17a8124932df5daaa47b7582bd6b8
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-03-17 16:09:45 +0530

    Update __init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,41 +1,29 @@
-# using flask_restful
+# Using flask to make an api
+# import necessary libraries and functions
 from flask import Flask, jsonify, request
-from flask_restful import Resource, Api
 
-# creating the flask app
+# creating a Flask app
 app = Flask(__name__)
-# creating an API object
-api = Api(app)
 
 
-# making a class for a particular resource
-# the get, post methods correspond to get and post requests
-# they are automatically mapped by flask_restful.
-# other methods include put, delete, etc.
-class Hello(Resource):
-
-    # corresponds to the GET request.
-    # this function is called whenever there
-    # is a GET request for this resource
-    def get(self):
-        return jsonify({'message': 'hello world'})
-
-    # Corresponds to POST request
-    def post(self):
-        data = request.get_json()  # status code
-        return jsonify({'data': data}), 201
+# on the terminal type: curl http://127.0.0.1:5000/
+# returns hello world when we use GET.
+# returns the data that we send when we use POST.
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        data = "hello world"
+        return jsonify({'data': data})
 
 
-# another resource to calculate the square of a number
-class Square(Resource):
+# A simple function to calculate the square of a number
+# the number to be squared is sent in the URL when we use GET
+# on the terminal type: curl http://127.0.0.1:5000 / home / 10
+# this returns 100 (square of 10)
+@app.route('/home/<int:num>', methods=['GET'])
+def disp(num):
+    return jsonify({'data': num ** 2})
 
-    def get(self, num):
-        return jsonify({'square': num ** 2})
-
-
-# adding the defined resources along with their corresponding urls
-api.add_resource(Hello, '/')
-api.add_resource(Square, '/square/<int:num>')
 
 # driver function
 if __name__ == '__main__':
